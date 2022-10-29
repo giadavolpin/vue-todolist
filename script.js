48eafaad1f824f62f195c41dbf375ea94f030da4
giadavolpin <volpingiada@gmail.com>
2022-10-29 15:42:22 +0200

    js

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-/* Rifare l'esercizio della to do list argomento dell to-do list a piacere.
-Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
-- text, una stringa che indica il testo del todo
-- done, un booleano (true/false) che indica se il todo è stato fatto oppure no
-MILESTONE 1
+/* MILESTONE 1
 Stampare all'interno di una lista, un item per ogni todo.
 Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
 MILESTONE 2
-Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
+Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, 
+il todo viene rimosso dalla lista.
 MILESTONE 3
 Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante,
 il testo digitato viene letto e utilizzato per creare un nuovo todo, 
 che quindi viene aggiunto alla lista dei todo esistenti.
 */
-/* const {createApp} = Vue; */
-import {createApp} from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+// far verificare se (if) è vero o falso 
+
+
+const {createApp} = Vue; 
+
 let done = true;
 
 const app = createApp({
     data(){
             return{
                 lista: [
-                {
+                {  //todo
                     testo: 'jumpin jack',
                     done: false, 
                 },           
@@ -55,9 +55,4 @@ const app = createApp({
             }
         }
 });
-
-/* mounted(){
-    console.log('in mounted')
-} */
-
-
+app.mount("#app");
